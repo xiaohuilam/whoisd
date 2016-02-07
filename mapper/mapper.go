3137c79d55f9aa1d726cd1846de3c1bf3d30dc6e
Igor Dolzhikov <bluesriverz@gmail.com>
2016-02-07 23:31:52 +0600

    remove json tags

--- mapper/mapper.go
+++ mapper/mapper.go
@@ -9,49 +9,49 @@ type Bundle []Entry
 
 type Entry struct {
 	// TLDs - list of TLDs, which accepted by specified Entry
-	TLDs []string `json:"TLDs"`
+	TLDs []string
 
 	// Default - use this entry as default for all undefined TLDs
-	Default bool `json:"default"`
+	Default bool
 
 	// a list of fields from "01" to last number "nn" in ascending order
-	Fields map[string]Field `json:"fields"`
+	Fields map[string]Field
 }
 
 // Field - representation of one field
 type Field struct {
 	// a label in whois output
-	Key string `json:"key"`
+	Key string
 
 	// used if a field has constant value
-	Value []string `json:"value"`
+	Value []string
 
 	// a name of the field in the database, if "value" is not defined
-	Name []string `json:"name"`
+	Name []string
 
 	// special instructions to indicate how to display the field
-	Format string `json:"format"`
+	Format string
 
 	// if this option is set to 'true', each value will be repeated in whois output with the same label
-	Multiple bool `json:"multiple"`
+	Multiple bool
 
 	// if this option is set to 'true', a value of the field will not shown in whois output
-	Hide bool `json:"hide"`
+	Hide bool
 
 	// a name of the field in a database through which a request for
-	Related string `json:"related"`
+	Related string
 
 	// a name of the field in a database through which related a request for
-	RelatedBy string `json:"related_by"`
+	RelatedBy string
 
 	// a name of the table/type in a database through which made a relation
-	RelatedTo string `json:"related_to"`
+	RelatedTo string
 
 	// it contains the number of field that replaces this field.
 	// If the field is referenced by "replacedBy" has non-empty value,
 	// then this field will not show in whois output, because this field
 	// should be replaced by the field specified in 'ReplacedBy'
-	ReplacedBy string `json:"replaced_by"`
+	ReplacedBy string
 }
 
 func (bundle Bundle) EntryByTLD(TLD string) *Entry {
